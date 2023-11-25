612908db1d9af443a3af5c47bce4c37462c4b8a0
Oficialjonathanece <bcjonathan99@gmail.com>
2023-11-24 22:22:58 -0500

    INSERT bucle infinito

---
+++ bucle.py
@@ -0,0 +1,11 @@
+import readchar
+
+#Bucle infinito hasta presionar el UP
+
+while True:
+    caracter = readchar.readkey()
+    print(caracter)
+    if ord(caracter) == "UP":
+        break
+
+    
